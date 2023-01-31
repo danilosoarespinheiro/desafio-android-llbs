eab7be01a8858344d7cd8a3e76a96cb9eee0c516
Matheus Mendes <matheus.mendes@luizalabs.com>
2023-01-31 09:43:52 -0300

    chore: board item layout improvement

--- app/src/main/java/com/example/desafioandroid/BoardItemViewHolder.kt
+++ app/src/main/java/com/example/desafioandroid/BoardItemViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.desafioandroid
 
+import android.graphics.Color
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_board_item.view.*
@@ -12,5 +13,11 @@ class BoardItemViewHolder(
         val position = adapterPosition + 1
         itemView.boardPosition.text = "$position"
         itemView.boardName.text = boardItem.name
+
+        if (boardItem.closed) {
+            itemView.cardView.setCardBackgroundColor(Color.LTGRAY)
+        } else {
+            itemView.cardView.setCardBackgroundColor(Color.WHITE)
+        }
     }
 }
